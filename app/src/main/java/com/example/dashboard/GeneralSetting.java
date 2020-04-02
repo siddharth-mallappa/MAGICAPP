e1f381c001ecf63181aa8312116b7a2727503d19
siddharth-mallappa <siddharth07sid@gmail.com>
2020-04-02 10:49:22 +0530

    Genral settings complete

--- app/src/main/java/com/example/dashboard/GeneralSetting.java
+++ app/src/main/java/com/example/dashboard/GeneralSetting.java
@@ -2,13 +2,92 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class GeneralSetting extends AppCompatActivity {
+import com.marcinmoskala.arcseekbar.ArcSeekBar;
+import com.marcinmoskala.arcseekbar.ProgressListener;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class GeneralSetting extends AppCompatActivity implements View.OnClickListener {
+    private RequestQueue mRequestQueue;
+    private StringRequest stringRequest;
+    private static final String TAG = GeneralSetting.class.getName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_setting);
+
+        Button restart = (Button) findViewById(R.id.button_restart);
+        Button shutdown = (Button) findViewById(R.id.button_shutdown);
+        Button refresh = (Button) findViewById(R.id.button_refresh);
+
+        restart.setOnClickListener(this);
+        shutdown.setOnClickListener(this);
+        refresh.setOnClickListener(this);
+
+        ArcSeekBar gradientseekbar = (ArcSeekBar) findViewById(R.id.seekbar);
+
+        int[] intArray = getResources().getIntArray(R.array.gradient);
+        gradientseekbar.setProgressGradient(intArray);
+
+        gradientseekbar.setOnProgressChangedListener(new ProgressListener() {
+            @Override
+            public void invoke(int i) {
+                Log.d("VALUE", "" + i);
+                String newstr = "http://10.0.2.2:8080/api/brightness/" + i;
+                sendANDprintResponce(newstr);
+            }
+        });
+
+
+    }
+
+    private void sendANDprintResponce(String url) {
+        mRequestQueue = Volley.newRequestQueue(this);
+
+
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG, "Responce" + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "ERROR" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button_refresh:
+                sendANDprintResponce("http://10.0.2.2:8080/api/refresh");
+                break;
+            case R.id.button_restart:
+                sendANDprintResponce("http://10.0.2.2:8080/api/reboot");
+                break;
+
+            case R.id.button_shutdown:
+                sendANDprintResponce("http://10.0.2.2:8080/api/shutdown");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/dashboard/MainActivity.java
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -9,8 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    public CardView newsfeed,notifications;
-    public CardView generalSetting;
+    public CardView newsfeed,notifications,generalSetting,account,compliments,calendar,clock,currentweather;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +22,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         newsfeed = (CardView)findViewById(R.id.click_newsfeed);
         generalSetting = (CardView)findViewById(R.id.click_general_setting);
         notifications=(CardView)findViewById(R.id.click_notification);
+        account=(CardView)findViewById(R.id.click_account);
+        compliments=(CardView)findViewById(R.id.click_compliments);
+        calendar=(CardView)findViewById(R.id.click_calander);
+        clock=(CardView)findViewById(R.id.click_clock);
+        currentweather=(CardView)findViewById(R.id.click_currentweather);
+
+
 
         newsfeed.setOnClickListener(this);
         notifications.setOnClickListener(this);
+        generalSetting.setOnClickListener(this);
+        account.setOnClickListener(this);
+        compliments.setOnClickListener(this);
+        calendar.setOnClickListener(this);
+        clock.setOnClickListener(this);
+        currentweather.setOnClickListener(this);
+
 
 
 
 
     }
-
-
-
-
 
 
     @Override
@@ -47,6 +57,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             break;
 
             case R.id.click_notification: i=new Intent(this,Notifications.class);
+                startActivity(i);
+                break;
+
+            case R.id.click_general_setting: i=new Intent(this,GeneralSetting.class);
+            startActivity(i);
+            break;
+
+            case R.id.click_calander: i=new Intent(this,Calendar.class);
+                startActivity(i);
+                break;
+
+            case R.id.click_clock: i=new Intent(this,Clock.class);
+                startActivity(i);
+                break;
+
+            case R.id.click_compliments: i=new Intent(this,Compliments.class);
+                startActivity(i);
+                break;
+
+            case R.id.click_currentweather: i=new Intent(this,CurrentWeather.class);
+                startActivity(i);
+                break;
+
+            case R.id.click_account: i=new Intent(this,Account.class);
                 startActivity(i);
                 break;
         }
